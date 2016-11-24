a7c5228ba42e7bd4f4d2f05ecdb1c6f8e6d758f1
Huachao Huang <huachao.huang@gmail.com>
2016-11-24 19:39:58 +0800

    server/coordinator: replace balancer worker with coordinator
    Coordinator can control the speed of different schedulers.
    
    Every scheduler has a unique name, we can add API to run or stop any
    schedulers dynamically, and different schedulers can run concurrently.

--- server/balancer.go
+++ server/balancer.go
@@ -129,6 +129,10 @@ func newReplicaChecker(cluster *clusterInfo, opt *scheduleOption) *replicaChecke
 	}
 }
 
+func (r *replicaChecker) GetResourceKind() ResourceKind {
+	return r.kind
+}
+
 func (r *replicaChecker) Check(region *regionInfo) *balanceOperator {
 	var stores []*storeInfo
 
